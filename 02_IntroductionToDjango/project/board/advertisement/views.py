b4304aa65aedc4987c82549a1ce69af0026d6f89
Eugene Vereshchagin <veresha23@gmail.com>
2022-10-04 22:18:11 +0300

    finished homework 3

--- 02_IntroductionToDjango/project/board/advertisement/views.py
+++ 02_IntroductionToDjango/project/board/advertisement/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 
 
 class Regions(TemplateView):
--- 03_RequestsHandlingInDjango/board/board/settings.py
+++ 03_RequestsHandlingInDjango/board/board/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'board.middleware.logging_users.LoggingUsers'
 ]
 
 ROOT_URLCONF = 'board.urls'
