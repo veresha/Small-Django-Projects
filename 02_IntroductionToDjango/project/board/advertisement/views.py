16fd4e4f645df1017a50476bedeb6068e276683a
Eugene Vereshchagin <veresha23@gmail.com>
2022-10-04 16:22:38 +0300

    do practice

--- 02_IntroductionToDjango/project/board/advertisement/views.py
+++ 02_IntroductionToDjango/project/board/advertisement/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 from django.http import HttpResponse
+
+
+class Regions(TemplateView):
+    template_name = 'advertisement/regions.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Бесплатные объявления в вашем городе'
+        context['title'] = 'Бесплатные объявления'
+        return context
+
+    def post(self, request):
+        mes = 'регион успешно создан'
+        return render(request, 'advertisement/regions.html', {'mes': mes})
 
 
 def advertisement_list(request, *args, **kwargs):
