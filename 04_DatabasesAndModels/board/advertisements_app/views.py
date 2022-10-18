5674af143fc9601989a775a8bf4d603a3e9fd89e
Eugene Vereshchagin <veresha23@gmail.com>
2022-10-18 17:47:52 +0300

    Maked views count

--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,6 +1,5 @@
 from django.views import generic
 from advertisements_app.models import Advertisement
-from django.shortcuts import render
 
 
 class AdvertisementListView(generic.ListView):
@@ -14,15 +13,6 @@ class AdvertisementDetailView(generic.DetailView):
     model = Advertisement
 
     def get_object(self, queryset=None):
-        Advertisement.views_plus()
-
-
-# class AdvCount(generic.View):
-#     count = 1
-#
-#     def get(self, request):
-#         AdvCount.count += 1
-#         return render(
-#          request=request, template_name='advertisements_app/advertisement_detail',
-#          context={'views_count': AdvCount.count}
-#         )
+        item = super().get_object(queryset)
+        item.views_plus()
+        return item
