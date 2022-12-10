df5361c73e7a10e07816d0e87a3c52ff580ca705
veresha <99193321+veresha@users.noreply.github.com>
2022-12-10 15:30:21 +0300

    Delete 02_IntroductionToDjango directory

--- 02_IntroductionToDjango/project/my_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\veres\PycharmProjects\python_django\project\my_venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
